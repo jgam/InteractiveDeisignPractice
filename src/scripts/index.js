b6c9fca429c027ff85c028dc9bdb27f2b8849d03
jgam <jgam@alumni.nd.edu>
2021-03-06 21:42:37 +0900

    [FEAT] basic interactive design

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,30 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 console.log('webpack starterkit');
+
+let x = 0;
+let y = 0;
+let cursor_item = document.getElementsByClassName("cursor_item")[0];
+let mx = 0;
+let my = 0;
+let speed = 0.03;
+
+window.onload = function() {
+  let h1 = document.getElementsByTagName("h1")[0];
+  window.addEventListener("click", mouseFunc, false);
+
+  function mouseFunc(e) {
+    x = e.clientX;
+    y = e.clientY;
+    h1.innerHTML = "x : " + e.clientX + "y: " + e.clientY;
+    console.log('mouse moved!!');
+  }
+  loop();
+};
+
+function loop(){
+  mx += (x-mx) * speed;
+  my += (y-my) * speed;
+  cursor_item.style.transform = "translate("+mx +"px,"+my+ "px)";
+  window.requestAnimationFrame(loop);
+}
