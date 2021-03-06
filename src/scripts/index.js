2f5cd4b3a8fd9259f8a02d287ebfb334be5853a2
jgam <jgam@alumni.nd.edu>
2021-03-06 23:49:07 +0900

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,31 +4,45 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
-
 let x = 0;
 let y = 0;
-let cursor_item = document.getElementsByClassName("cursor_item")[0];
 let mx = 0;
 let my = 0;
-let speed = 0.03;
+let speed = 0.1;
+
+let h1;
+let first;
+let second;
+let third;
+let fourth;
+let vod;
 
 window.onload = function() {
-  let h1 = document.getElementsByTagName("h1")[0];
-  window.addEventListener("click", mouseFunc, false);
+  h1 = document.getElementsByTagName("h1")[0];
+  first = document.getElementsByClassName("first")[0];
+  second = document.getElementsByClassName("second")[0];
+  third = document.getElementsByClassName("third")[0];
+  fourth = document.getElementsByClassName("fourth")[0];
+  vod = document.getElementsByClassName("vod")[0];
+
+  window.addEventListener("mousemove", mouseFunc, false);
 
   function mouseFunc(e) {
     x = e.clientX;
     y = e.clientY;
-    h1.innerHTML = "x : " + e.clientX + "y: " + e.clientY;
-    console.log('mouse moved!!');
   }
   loop();
 };
 
 function loop(){
-  mx += (x-mx) * speed;
-  my += (y-my) * speed;
-  cursor_item.style.transform = "translate("+mx +"px,"+my+ "px)";
+  mx += (x - mx) * speed;
+  my += (y - my) * speed;
+
+  h1.innerHTML = "x : " + x + "y: " + y;
+  second.style.transform = "translate("+mx/120 +"px,"+my/120+ "px)";
+  third.style.transform = "translate("+mx/-10 +"px,"+my/-10+ "px)";
+  fourth.style.transform = "translate("+mx/-7 +"px,"+my/-7+ "px)";
+  vod.style.transform = "translate("+mx/-70 +"px,"+my/-70+ "px)";
+
   window.requestAnimationFrame(loop);
 }
